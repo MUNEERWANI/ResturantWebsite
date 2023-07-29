3b8af5e60a918ed4e6d4dc11909e1bd31a084eec
Muneer ahmad wani <63859657+MUNEERWANI@users.noreply.github.com>
2023-07-29 18:52:27 +0530

    Add files via upload

---
+++ Modal.js
@@ -0,0 +1,27 @@
+import classes from './Modal.module.css';
+import { Fragment } from 'react';
+import ReactDom from 'react-dom'
+const Backdrop=(props)=>{
+return(
+    <div className={classes.backdrop}></div>
+) 
+}
+const ModalOverlay=(props)=>{
+    return (<div className={classes.modal}> 
+    <div className={classes.content}>{props.children}</div>
+
+
+        </div>)
+
+}
+const Modal=(props)=>{
+    const portalElement=document.getElementById('overlays')
+    return(
+        <Fragment>
+            {ReactDom.createPortal(<Backdrop />,portalElement)}
+            {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
+        </Fragment>
+         
+    )
+}
+export default Modal;
